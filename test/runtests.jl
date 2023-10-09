042f3de32d7ecbfc19272357b5d739395d69dd28
Tae Yoon (Harry) Lee <taeyoon.harry.lee@gmail.com>
2023-10-09 16:26:52 -0700

    fix verbose

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ using Setfield, DataFrames
 using Statistics, Distributions, StatsBase, SpecialFunctions
 using Distributed
 
-simulation = LEAP.set_up(111,"CA",2001,40,100,"M3");
-run_test= LEAP.process(simulation,1,false,true);
+simulation = LEAP.set_up(111,"CA",2001,40,10,"M3");
+run_test= LEAP.process(simulation,1,false,false);
