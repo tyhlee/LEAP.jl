e5a2004475fe92fe7bb73c238e50c12c1d20f263
Ainsleigh <ainsleighhill@gmail.com>
2023-11-06 11:55:27 -0800

    add docstrings for antibioticExposure

--- src/modules/antibioticExposure.jl
+++ src/modules/antibioticExposure.jl
@@ -1,52 +1,124 @@
 # include("abstractModule.jl")
 # include("../utils")
 
+"""
+    AntibioticExposure
+
+A struct for antibiotic exposure.
+
+# Fields
+- `hyperparameters::Union{AbstractDict, Nothing}`: A dictionary containing two keys,
+    `β0_μ` and `β0_σ`.
+- `parameters::Union{AbstractDict,Nothing}`: A dictionary containing the following keys:
+    `θ`, `β0`, `βage`, `βsex`, `βcal_year`, `β2005`, `β2005_cal_year`, `fixyear`, `fix2000`,
+    `βfloor`, `midtrends`.
+- `AbxOR: TODO.
+"""
 struct AntibioticExposure <: AntibioticExposure_Module
     hyperparameters::AbstractDict
     parameters::AbstractDict
     AbxOR
 end
 
-function process(ag::Agent,anti::AntibioticExposure)
-    if !isnothing(anti.parameters[:fixyear])
-        if isa(anti.parameters[:fixyear],Number)
-            rand(NegativeBinomial(anti.parameters[:θ], antibioticExposure_prob(ag.sex,anti.parameters[:fixyear],anti.parameters)))
+
+"""
+    process(agent, antibio)
+
+Return a random value from a negative binomial distribution.
+
+# Arguments
+- `agent::Agent`: A person in the model, see [`Agent`](@ref).
+- `antibio::AntibioticExposure`: AntibioticExposure module, see [`AntibioticExposure`](@ref).
+
+"""
+function process(agent::Agent, antibio::AntibioticExposure)
+    if !isnothing(antibio.parameters[:fixyear])
+        if isa(antibio.parameters[:fixyear], Number)
+            r = antibio.parameters[:θ]
+            p = antibiotic_exposure_prob(
+                agent.sex, antibio.parameters[:fixyear], antibio.parameters
+            )
         else
-            tmp_mu = max(anti.parameters[:midtrends][(ag.cal_year,Int(ag.sex))].rate[1],anti.parameters[:βfloor])
-            rand(NegativeBinomial(anti.parameters[:θ], anti.parameters[:θ] / (anti.parameters[:θ] + tmp_mu)))
+            tmp_mu = max(
+                antibio.parameters[:midtrends][(agent.cal_year, Int(agent.sex))].rate[1],
+                antibio.parameters[:βfloor]
+            )
+            r = anti.parameters[:θ]
+            p = anti.parameters[:θ] / (antibio.parameters[:θ] + tmp_mu)
         end
     else
-        rand(NegativeBinomial(anti.parameters[:θ], antibioticExposure_prob(ag.sex,ag.cal_year,anti.parameters)))
+        r = anti.parameters[:θ]
+        p = antibiotic_exposure_prob(agent.sex, agent.cal_year, antibio.parameters)
     end
-
+    return rand(NegativeBinomial(r, p))
 end
 
-function process_initial(ag::Agent,anti::AntibioticExposure,cal_born)
+
+"""
+    process_initial(agent, antibio)
+
+Return a random value from a negative binomial distribution.
+
+# Arguments
+- `agent::Agent`: A person in the model, see [`Agent`](@ref).
+- `antibio::AntibioticExposure`: AntibioticExposure module, see [`AntibioticExposure`](@ref).
+
+"""
+function process_initial(agent::Agent, antibio::AntibioticExposure, cal_born::Integer)
     if cal_born < 2001
-        rand(NegativeBinomial(anti.parameters[:θ], antibioticExposure_prob(ag.sex,2000,anti.parameters))) 
+        r = antibio.parameters[:θ]
+        p = antibiotic_exposure_prob(
+            agent.sex, 2000, antibio.parameters
+        )
     else
-        if !isnothing(anti.parameters[:fixyear])
-            if isa(anti.parameters[:fixyear],Number)
-                rand(NegativeBinomial(anti.parameters[:θ], antibioticExposure_prob(ag.sex,anti.parameters[:fixyear],anti.parameters))) 
-            else 
-                tmp_mu = max( anti.parameters[:midtrends][(cal_born,Int(ag.sex))].rate[1],anti.parameters[:βfloor])
-                rand(NegativeBinomial(anti.parameters[:θ], anti.parameters[:θ] / (anti.parameters[:θ] + tmp_mu)))
+        if !isnothing(antibio.parameters[:fixyear])
+            if isa(antibio.parameters[:fixyear], Number)
+                r = antibio.parameters[:θ]
+                p = antibiotic_exposure_prob(
+                    agent.sex, antibio.parameters[:fixyear], antibio.parameters
+                )
+            else
+                tmp_mu = max(
+                    antibio.parameters[:midtrends][(cal_born, Int(agent.sex))].rate[1],
+                    antibio.parameters[:βfloor]
+                )
+                r = anti.parameters[:θ]
+                p = anti.parameters[:θ] / (antibio.parameters[:θ] + tmp_mu)
             end
         else
-            rand(NegativeBinomial(anti.parameters[:θ], antibioticExposure_prob(ag.sex,cal_born,anti.parameters))) 
+            r = anti.parameters[:θ]
+            p = antibiotic_exposure_prob(agent.sex, cal_born, antibio.parameters)
         end
     end
+    return rand(NegativeBinomial(r, p))
 end
 
-# helper function for above
-function antibioticExposure_prob(sex::Bool,cal_year,parameters::AbstractDict)
-    mu = exp(parameters[:β0] + parameters[:βsex] * sex + parameters[:βcal_year] * cal_year + 
-    parameters[:β2005] * (cal_year>2005) +  parameters[:β2005_cal_year] * (cal_year>2005)*cal_year)
-    mu = max(mu,parameters[:βfloor]/1000)
-    parameters[:θ] / (parameters[:θ] + mu)
+
+"""
+    antibiotic_exposure_prob(sex, cal_year, parameters)
+
+Returns the probability of antibiotic exposure for a given year and sex.
+
+# Arguments
+- `sex::Bool`: Sex of agent, true = male, false = female.
+- `cal_year::Integer`: The calendar year.
+- `parameters::AbstractDict`: A dictionary containing the following keys:
+    `θ`, `β0`, `βage`, `βsex`, `βcal_year`.
+
+"""
+function antibiotic_exposure_prob(sex::Bool, cal_year::Integer, parameters::AbstractDict)
+    μ = (
+        exp(parameters[:β0] +
+        parameters[:βsex] * sex +
+        parameters[:βcal_year] * cal_year +
+        parameters[:β2005] * (cal_year > 2005) +
+        parameters[:β2005_cal_year] * (cal_year > 2005) * cal_year
+    )
+    μ = max(μ, parameters[:βfloor] / 1000)
+    return parameters[:θ] / (parameters[:θ] + μ)
 end
 
-function random_parameter_initialization!(anti::AntibioticExposure)
+function random_parameter_initialization!(antibio::AntibioticExposure)
     # anti.parameters[:β0] = rand(Normal(anti.hyperparameters[:β0_μ], anti.hyperparameters[:β0_σ]))
     nothing
 end
