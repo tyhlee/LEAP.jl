420c7dd0567c4d5679c66e83e1ae256e734e9115
Tae Yoon (Harry) Lee <taeyoon.harry.lee@gmail.com>
2024-05-30 08:41:38 -0700

    remove reliance on semi processed data

--- R/2.initial_population.R
+++ R/2.initial_population.R
@@ -71,16 +71,20 @@ initial_pop_distribution_prop %>%
   mutate(prop=n/n_birth) %>% 
   left_join(initial_pop_distribution_both,by=c("year","age","province"))-> initial_pop_distribution_prop_past
 
-initial_pop <- readRDS(here("public_dataset","pop_projection_BC_CA.rds")) %>% 
-  filter(GEO %in% c('Canada',"British Columbia")) %>% 
-  filter(REF_DATE >= max(initial_pop_distribution_prop$year)) %>% 
+
+initial_pop <- read_csv(here("public_dataset","17100057.csv"))
+colnames(initial_pop) <- gsub(' ','_',colnames(initial_pop))
+initial_pop_distribution <- initial_pop %>%
+  select(REF_DATE,GEO,Projection_scenario,Sex,Age_group,VALUE) %>% 
   mutate(Projection_scenario = str_remove(Projection_scenario,"Projection scenario "),
          Projection_scenario = str_remove(Projection_scenario, "\\:.*")) %>% 
+  filter(GEO %in% c('Canada',"British Columbia")) %>%
+  filter(REF_DATE >= max(initial_pop_distribution_prop$year)) %>%
+  mutate(Projection_scenario = str_remove(Projection_scenario,"Projection scenario "),
+         Projection_scenario = str_remove(Projection_scenario, "\\:.*")) %>%
   mutate(sex=str_sub(Sex,1,1),
          age_remove = str_detect(Age_group,'to|over|All|Median|Average'),
-         age_keep = Age_group=="100 years and over")
-
-initial_pop_distribution <- initial_pop %>% 
+         age_keep = Age_group=="100 years and over") %>% 
   filter(!age_remove | age_keep) %>%
   mutate(age = as.numeric(gsub("([0-9]+).*$","\\1",Age_group)),
          age = ifelse(is.na(age),0,age)) %>% 
@@ -95,6 +99,34 @@ initial_pop_distribution <- initial_pop %>%
                               TRUE ~ "NONE")) %>% 
   filter(sex!="B") %>% 
   filter(!is.na(n))
+
+
+
+
+# initial_pop <- readRDS(here("public_dataset","pop_projection_BC_CA.rds")) %>% 
+#   filter(GEO %in% c('Canada',"British Columbia")) %>% 
+#   filter(REF_DATE >= max(initial_pop_distribution_prop$year)) %>% 
+#   mutate(Projection_scenario = str_remove(Projection_scenario,"Projection scenario "),
+#          Projection_scenario = str_remove(Projection_scenario, "\\:.*")) %>% 
+#   mutate(sex=str_sub(Sex,1,1),
+#          age_remove = str_detect(Age_group,'to|over|All|Median|Average'),
+#          age_keep = Age_group=="100 years and over")
+# 
+# initial_pop_distribution <- initial_pop %>% 
+#   filter(!age_remove | age_keep) %>%
+#   mutate(age = as.numeric(gsub("([0-9]+).*$","\\1",Age_group)),
+#          age = ifelse(is.na(age),0,age)) %>% 
+#   rename(n=VALUE,
+#          projection_scenario = Projection_scenario ) %>% 
+#   select(1,projection_scenario,sex,age,GEO,n) %>% 
+#   rename(year=REF_DATE,
+#          province=GEO) %>% 
+#   mutate(n=n*1000) %>% 
+#   mutate(province = case_when(province=="Canada" ~ "CA",
+#                               province=="British Columbia" ~ "BC",
+#                               TRUE ~ "NONE")) %>% 
+#   filter(sex!="B") %>% 
+#   filter(!is.na(n))
 
 # note that for province, projections are not available 2044 onwards
 
